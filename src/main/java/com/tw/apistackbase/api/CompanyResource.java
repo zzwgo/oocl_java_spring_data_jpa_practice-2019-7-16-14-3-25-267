9a0c055782be743691e7a4ac065195eeae6f412e
aaron <kenzzw@163.com>
2019-07-17 15:14:09 +0800

    Add: init and add CompanyResource list and add method

--- src/main/java/com/tw/apistackbase/api/CompanyResource.java
+++ src/main/java/com/tw/apistackbase/api/CompanyResource.java
@@ -1,19 +1,28 @@
 package com.tw.apistackbase.api;
 
 import com.tw.apistackbase.core.Company;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import com.tw.apistackbase.repository.CompanyRepository;
 
 @RestController
 @RequestMapping("/companies")
 public class CompanyResource {
-    
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
     @GetMapping(produces = {"application/json"})
     public Iterable<Company> list() {
-        return null;
+        return companyRepository.findAll();
     }
     
     @PostMapping(produces = {"application/json"})
     public Company add(@RequestBody Company company) {
-        return null;
+        return companyRepository.save(company);
     }
+
+
+
+
 }
--- src/main/java/com/tw/apistackbase/core/Company.java
+++ src/main/java/com/tw/apistackbase/core/Company.java
@@ -1,6 +1,14 @@
 package com.tw.apistackbase.core;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Company {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
     private Long id;
 
     private String name;
